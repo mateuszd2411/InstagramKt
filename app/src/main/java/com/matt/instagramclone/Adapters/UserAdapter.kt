ef0226d3f6a84b853ca137f2c2a4f86cdb1befcd
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-19 13:04:39 +0200

    Set follow button clickable. Added logic for clicking "Follow" button. Change to "Following", "Followers" , "Follow"

--- app/src/main/java/com/matt/instagramclone/Adapters/UserAdapter.kt
+++ app/src/main/java/com/matt/instagramclone/Adapters/UserAdapter.kt
@@ -8,6 +8,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import com.matt.instagramclone.Models.User
 import com.matt.instagramclone.R
 import com.squareup.picasso.Picasso
@@ -22,6 +24,7 @@ class UserAdapter (private var mContext: Context,
 
         return UserAdapter.ViewHolder(view)
     }
+    private var firebaeUser: FirebaseUser ?= null
 
     override fun getItemCount(): Int {
         return mUser.size
@@ -33,6 +36,54 @@ class UserAdapter (private var mContext: Context,
         holder.userNameTextView.text = user.getUsername()
         holder.userFullnameTextView.text = user.getFullname()
         Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(holder.userProfileImage)
+
+        holder.followButton.setOnClickListener {
+            if (holder.followButton.text.toString() == "Follow") {
+                firebaeUser?.uid.let { it1 ->
+                    FirebaseDatabase.getInstance().reference
+                        .child("Follow").child(it1.toString())
+                        .child("Following").child(user.getUid())
+                        .setValue(true).addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                firebaeUser?.uid.let { it1 ->
+                                    FirebaseDatabase.getInstance().reference
+                                        .child("Follow").child(user.getUid())
+                                        .child("Followers").child(it1.toString())
+                                        .setValue(true).addOnCompleteListener { task ->
+                                            if (task.isSuccessful) {
+
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                }
+            }
+            else {
+                firebaeUser?.uid.let { it1 ->
+                    FirebaseDatabase.getInstance().reference
+                        .child("Follow").child(it1.toString())
+                        .child("Following").child(user.getUid())
+                        .removeValue().addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+
+                                firebaeUser?.uid.let { it1 ->
+                                    FirebaseDatabase.getInstance().reference
+                                        .child("Follow").child(user.getUid())
+                                        .child("Followers").child(it1.toString())
+                                        .removeValue().addOnCompleteListener { task ->
+                                            if (task.isSuccessful) {
+
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                }
+            }
+        }
+
     }
 
     class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
