1191d2ba89877df3bbeb5a364a9d96766d349e99
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-19 20:05:50 +0200

    User can change profile image. Upload image to database storage, progress dialog

--- app/src/main/java/com/matt/instagramclone/AccountSettingsActivity.kt
+++ app/src/main/java/com/matt/instagramclone/AccountSettingsActivity.kt
@@ -1,12 +1,16 @@
 package com.matt.instagramclone
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -15,11 +19,12 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.matt.instagramclone.Models.User
 import com.squareup.picasso.Picasso
 import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_account_settings.*
-import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class AccountSettingsActivity : AppCompatActivity() {
 
@@ -167,8 +172,60 @@ class AccountSettingsActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG
             ).show()
 
-            else ->{
-                val fileref = storageProfilePicRef!!.child(firebaseUser!!.uid + ".jpg")
+            else -> {
+
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Account Settings")
+                progressDialog.setMessage("Please wait, we are updating your profile...")
+                progressDialog.show()
+
+                val fileRef = storageProfilePicRef!!.child(firebaseUser!!.uid + ".jpg")
+
+                var uploadTask: StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+                uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                            progressDialog.dismiss()
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                }).addOnCompleteListener (OnCompleteListener<Uri> { task ->
+                    if (task.isSuccessful) {
+                        val downloadUrl = task.result
+                        myUrl = downloadUrl.toString()
+
+                        val ref = FirebaseDatabase.getInstance().reference.child("UsersKt")
+
+                        val userMap = HashMap<String, Any>()
+                        userMap["fullname"] =
+                            full_name_profile_frag.text.toString().toLowerCase()
+                        userMap["username"] =
+                            username_profile_frag.text.toString().toLowerCase()
+                        userMap["bio"] = bio_profile_frag.text.toString().toLowerCase()
+                        userMap["image"] = myUrl
+
+                        ref.child(firebaseUser.uid).updateChildren(userMap)
+
+                        Toast.makeText(
+                            this,
+                            "Account Information has been updated successfully...",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        val intent =
+                            Intent(this@AccountSettingsActivity, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                        progressDialog.dismiss()
+
+                    } else {
+                        progressDialog.dismiss()
+                    }
+                    }
+                )
             }
         }
 
