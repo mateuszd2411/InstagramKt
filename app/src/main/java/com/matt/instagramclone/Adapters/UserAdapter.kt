b2408b9d20da4865ecac2e11032c4c064617e7e7
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-19 13:40:10 +0200

    Changed "Follow" to "FollowKt" (child in firebase) + typo. Added checkFollowingStatus, now user can click to follow button and add user to follow list in database

--- app/src/main/java/com/matt/instagramclone/Adapters/UserAdapter.kt
+++ app/src/main/java/com/matt/instagramclone/Adapters/UserAdapter.kt
@@ -8,13 +8,16 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.matt.instagramclone.Models.User
 import com.matt.instagramclone.R
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
-import kotlinx.android.synthetic.main.user_item_layout.view.*
 
 class UserAdapter (private var mContext: Context,
                    private var mUser: List<User>,
@@ -24,7 +27,7 @@ class UserAdapter (private var mContext: Context,
 
         return UserAdapter.ViewHolder(view)
     }
-    private var firebaeUser: FirebaseUser ?= null
+    private var firebaseUser: FirebaseUser ?= FirebaseAuth.getInstance().currentUser
 
     override fun getItemCount(): Int {
         return mUser.size
@@ -37,18 +40,21 @@ class UserAdapter (private var mContext: Context,
         holder.userFullnameTextView.text = user.getFullname()
         Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(holder.userProfileImage)
 
+        checkFollowingStatus(user.getUid(), holder.followButton)
+
         holder.followButton.setOnClickListener {
+            //logic for switch follow button
             if (holder.followButton.text.toString() == "Follow") {
-                firebaeUser?.uid.let { it1 ->
+                firebaseUser?.uid.let { it1 ->
                     FirebaseDatabase.getInstance().reference
-                        .child("Follow").child(it1.toString())
+                        .child("FollowKt").child(it1.toString())
                         .child("Following").child(user.getUid())
                         .setValue(true).addOnCompleteListener { task ->
                             if (task.isSuccessful) {
 
-                                firebaeUser?.uid.let { it1 ->
+                                firebaseUser?.uid.let { it1 ->
                                     FirebaseDatabase.getInstance().reference
-                                        .child("Follow").child(user.getUid())
+                                        .child("FollowKt").child(user.getUid())
                                         .child("Followers").child(it1.toString())
                                         .setValue(true).addOnCompleteListener { task ->
                                             if (task.isSuccessful) {
@@ -61,16 +67,16 @@ class UserAdapter (private var mContext: Context,
                 }
             }
             else {
-                firebaeUser?.uid.let { it1 ->
+                firebaseUser?.uid.let { it1 ->
                     FirebaseDatabase.getInstance().reference
-                        .child("Follow").child(it1.toString())
+                        .child("FollowKt").child(it1.toString())
                         .child("Following").child(user.getUid())
                         .removeValue().addOnCompleteListener { task ->
                             if (task.isSuccessful) {
 
-                                firebaeUser?.uid.let { it1 ->
+                                firebaseUser?.uid.let { it1 ->
                                     FirebaseDatabase.getInstance().reference
-                                        .child("Follow").child(user.getUid())
+                                        .child("FollowKt").child(user.getUid())
                                         .child("Followers").child(it1.toString())
                                         .removeValue().addOnCompleteListener { task ->
                                             if (task.isSuccessful) {
@@ -87,9 +93,32 @@ class UserAdapter (private var mContext: Context,
     }
 
     class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         var userNameTextView: TextView = itemView.findViewById(R.id.user_name_search)
         var userFullnameTextView: TextView = itemView.findViewById(R.id.user_full_name_search)
         var userProfileImage: CircleImageView = itemView.findViewById(R.id.user_profile_image_search)
         var followButton: Button = itemView.findViewById(R.id.follow_btn_search)
+    }
+
+    private fun checkFollowingStatus(uid: String, followButton: Button) {
+        val followingRef = firebaseUser?.uid.let { it1 ->
+            FirebaseDatabase.getInstance().reference
+                .child("FollowKt").child(it1.toString())
+                .child("Following")
+        }
+
+        followingRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.child(uid).exists()) {
+                    followButton.text = "Following"
+                } else {
+                    followButton.text = "Follow"
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
     }
 }
