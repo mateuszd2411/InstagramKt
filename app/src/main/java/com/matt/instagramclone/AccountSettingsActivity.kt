7d97404c5ac74dbb5c2d97f04308e97f0a82e233
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-19 19:13:21 +0200

    Added library image cropper to gradle. Set CropImage, clickable change_image_text_btn. Started updateImageAndUpdateInfo()[+validation]

--- app/src/main/java/com/matt/instagramclone/AccountSettingsActivity.kt
+++ app/src/main/java/com/matt/instagramclone/AccountSettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.matt.instagramclone
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
@@ -11,8 +13,11 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.matt.instagramclone.Models.User
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import kotlinx.android.synthetic.main.activity_account_settings.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 
@@ -20,12 +25,16 @@ class AccountSettingsActivity : AppCompatActivity() {
 
     private lateinit var firebaseUser: FirebaseUser
     private var checker = ""
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+    private var storageProfilePicRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
 
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
+        storageProfilePicRef = FirebaseStorage.getInstance().reference.child("Profile Pictures")
 
         logout_btn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -36,15 +45,34 @@ class AccountSettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        change_image_text_btn.setOnClickListener {
+
+            checker = "clicked"
+
+            CropImage.activity()
+                .setAspectRatio(1, 1)
+                .start(this@AccountSettingsActivity)
+        }
+
         save_info_profile_btn.setOnClickListener {
             if (checker == "clicked") {
-
+                updateImageAndUpdateInfo()
             } else {
                 updateUserInfoOnly()
             }
         }
 
         userInfo()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            profile_image_view_profile_frag.setImageURI(imageUri)
+        }
     }
 
     private fun updateUserInfoOnly() {
@@ -114,4 +142,37 @@ class AccountSettingsActivity : AppCompatActivity() {
 
         })
     }
+
+    private fun updateImageAndUpdateInfo() {
+
+        when {
+            imageUri == null -> Toast.makeText(
+                this,
+                "Please select image first...",
+                Toast.LENGTH_LONG
+            ).show()
+            TextUtils.isEmpty(full_name_profile_frag.text.toString()) -> Toast.makeText(
+                this,
+                "Please write full name first...",
+                Toast.LENGTH_LONG
+            ).show()
+            username_profile_frag.text.toString() == "" -> Toast.makeText(
+                this,
+                "Please write user name first...",
+                Toast.LENGTH_LONG
+            ).show()
+            bio_profile_frag.text.toString() == "" -> Toast.makeText(
+                this,
+                "Please write your bio first...",
+                Toast.LENGTH_LONG
+            ).show()
+
+            else ->{
+                val fileref = storageProfilePicRef!!.child(firebaseUser!!.uid + ".jpg")
+            }
+        }
+
+
+    }
+
 }
