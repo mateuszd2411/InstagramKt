b1b80ff437199169285b31cbde962451b757b3f3
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-20 20:36:24 +0200

    Added recyclerView to HomeFragment (for displaying new posts)

--- app/src/main/java/com/matt/instagramclone/Fragments/HomeFragment.kt
+++ app/src/main/java/com/matt/instagramclone/Fragments/HomeFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.matt.instagramclone.R
 
@@ -19,7 +21,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        var recyclerView: RecyclerView? = null
+        recyclerView = view.findViewById(R.id.recycler_view_home)
+        val linearLayoutManager = LinearLayoutManager(context)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        recyclerView.layoutManager = linearLayoutManager
+
+        return view
     }
 
 
