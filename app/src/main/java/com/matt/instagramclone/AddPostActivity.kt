697ac09cb6c1557adf31b1fc0355dc2d03247d2a
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-20 10:10:04 +0200

    Added crop image in post to FirebaseStorage

--- app/src/main/java/com/matt/instagramclone/AddPostActivity.kt
+++ app/src/main/java/com/matt/instagramclone/AddPostActivity.kt
@@ -1,12 +1,46 @@
 package com.matt.instagramclone
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.activity_account_settings.*
+import kotlinx.android.synthetic.main.activity_add_post.*
 
 class AddPostActivity : AppCompatActivity() {
+
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+    private var storagePostPicRef: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_post)
+
+        storagePostPicRef = FirebaseStorage.getInstance().reference.child("Post Pictures")
+
+        save_new_post_btn.setOnClickListener { uploadImage() }
+
+        CropImage.activity()
+            .setAspectRatio(2, 1)
+            .start(this@AddPostActivity)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            image_post.setImageURI(imageUri)
+        }
+    }
+
+    private fun uploadImage() {
+
     }
 }
