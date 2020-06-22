f78796ef824b9931d30896e27d2143c813b10c67
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-22 12:38:21 +0200

    Set clickable post image below user info(in Profile) and go to post details (putString("postId")

--- app/src/main/java/com/matt/instagramclone/Adapters/MyImagesAdapter.kt
+++ app/src/main/java/com/matt/instagramclone/Adapters/MyImagesAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.NonNull
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.matt.instagramclone.Fragments.PostDetailsFragment
 import com.matt.instagramclone.Models.Post
 import com.matt.instagramclone.R
 import com.squareup.picasso.Picasso
@@ -34,6 +36,19 @@ class MyImagesAdapter(private val mContext: Context, mPost: List<Post>)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val post: Post = mPost!![position]
         Picasso.get().load(post.getPostimage()).into(holder.postImage)
+
+        holder.postImage.setOnClickListener {
+
+            val editor = mContext.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit()
+
+            editor.putString("postId", post.getPostid())
+
+            editor.apply()
+
+            (mContext as FragmentActivity).supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, PostDetailsFragment()).commit()
+        }
     }
 
     inner class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
