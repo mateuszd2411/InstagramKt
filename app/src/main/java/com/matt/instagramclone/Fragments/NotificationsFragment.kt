2206c1ae591b09356ccd5c7cb265810f24eeb79d
mateuszd2411 <mateuszd2411@gmail.com>
2020-06-23 11:37:22 +0200

    Set recyclerView in NotificationsFragment

--- app/src/main/java/com/matt/instagramclone/Fragments/NotificationsFragment.kt
+++ app/src/main/java/com/matt/instagramclone/Fragments/NotificationsFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.matt.instagramclone.Adapters.NotificationAdapter
+import com.matt.instagramclone.Models.Notifications
 
 import com.matt.instagramclone.R
 
@@ -14,12 +18,28 @@ import com.matt.instagramclone.R
  */
 class NotificationsFragment : Fragment() {
 
+    private var notificationList: List<Notifications>? = null
+    private var notificationAdapter: NotificationAdapter? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications, container, false)
+        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
+
+        var recyclerView: RecyclerView
+        recyclerView = view.findViewById(R.id.recycler_view_notifications)
+        recyclerView?.setHasFixedSize(true)
+        recyclerView?.layoutManager = LinearLayoutManager(context)
+
+        notificationList = ArrayList()
+
+        notificationAdapter =
+            NotificationAdapter(context!!, notificationList as ArrayList<Notifications>)
+        recyclerView.adapter = notificationAdapter
+
+        return view
     }
 
 
